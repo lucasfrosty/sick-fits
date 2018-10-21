93fceb590de95ac871275a03481f702e9a607fde
Lucas Ferreira <lucas_frosty@hotmail.com>
2018-10-21 14:48:30 -0400

    intro to Links

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function Home() {
+  return (
+    <>
+      <p>hey!!</p>
+      <Link href="/sell"><a>Sell</a></Link>
+    </>
+  );
+}
+
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function Sell() {
+  return (
+    <>
+      <h1>Sell :)</h1>
+      <Link href="/">Go back to home</Link>
+    </>
+  );
+}
+
+export default Sell;
